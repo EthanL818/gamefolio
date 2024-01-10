9a695a4174ff22f82a2a8d786a5af6aa78b1e714
Ethan Liu <ethanliu818@gmail.com>
2024-01-09 23:30:01 -0500

    Update token.js

--- public/token.js
+++ public/token.js
@@ -1,2 +1,4 @@
-const TOKEN = "bw345huofvypxs769yh6oucn3y9g4o"
+const TOKEN = "iu2hpoe5azli5jwhw8zxacxsssauvm"
 export default TOKEN;
+
+// when this breaks again, go to https://api-docs.igdb.com/#account-creation and follow the instructions in postman with new secret generated in twitch developer portal
